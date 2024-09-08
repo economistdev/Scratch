7b844be0d89945dc65308ad284c1c65457ac6b83
George C <georgecross98@gmail.com>
2024-09-08 23:29:22 +0100

    almost done with char functions chal 2

--- C/learning.c
+++ C/learning.c
@@ -1,52 +1,95 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>  // Include this header for toupper
 
-int main() {
+int main(int argc, char *argv[]) {
 
-    int ch = '\0';
-    size_t char_count = 1;
+    //argv will contain [filename, conversion_method["upper", "lower"] ]
+    size_t file_name_length = 0;
 
-    char filename[100];
-    size_t filename_size = 0;
+    char *file_name = '\0';
+    char *file_name_temp = '\0';
 
-    FILE * file_read = NULL;
-    FILE * file_write = NULL;
+    char *method = '\0';
 
-    printf("Please enter the file name you would like to open...\n");
-    while((ch = getc(stdin)) != '\n') {
-        filename[filename_size] = ch;
-        filename_size++;
-    }
-    filename[filename_size] = '\0';
+    int ch = 0;
 
-    // Try opening the file for reading
-    file_read = fopen(filename, "r");
-    if (file_read == NULL) {
-        printf("Error: Could not open file '%s'\n", filename);
+    FILE *file_input = NULL;
+    FILE *file_output = NULL;
+
+    if (argc > 1) {
+        file_name_length = strlen(argv[1]+1);
+        file_name = (char*)malloc(sizeof(char) * file_name_length);
+        if (file_name == NULL) {
+            printf("Memory allocation failed");
+            return 1;
+        }
+        file_input = fopen(argv[1], "r");
+
+        if (file_input == NULL) {
+            printf("Unable to open file");
+            return 1;
+        }
+
+        if (argc == 3) {
+            method = (char *)malloc( sizeof(char) * (strlen(argv[2])+1) );
+            if (method == NULL) {
+                printf("Memory allocation failed");
+                return 1;
+            }
+            strcpy(method, argv[2]);
+        } else {
+            char method = "upper";
+        }
+
+    } else {
+        printf("No filename was entered");
         return 1;
     }
 
-    // Open the output file for writing
-    file_write = fopen("output_file.txt", "w");
-    if (file_write == NULL) {
-        printf("Error: Could not create output file.\n");
-        fclose(file_read);
+    file_name_temp = (char*)malloc(sizeof(char) * (sizeof(file_name_length) + 5)); // adding _temp into the string
+    if (file_name_temp == NULL) {
+        printf("Memory allocation failed");
         return 1;
     }
 
-    ch = '\0';
-    while((ch = getc(file_read)) != EOF) {
-        putc(ch, file_write);
-        char_count += 1;
+    const char *dot = strrchr(file_name, '.');
 
+    size_t base_len = dot ? (size_t)(dot - file_name) : strlen(file_name);
+
+    strncpy(file_name_temp, file_name, base_len);
+    strcpy(file_name_temp, "_temp");
+
+    if (dot) {
+        strcpy(file_name_temp, dot);
     }
 
-    fclose(file_read);
-    fclose(file_write);
+    //creat the temp file
+    file_output = fopen(file_name_temp, "w");
 
-    printf("\nWe have a file a character count of: %zu\n", (char_count));
+    while ((ch = fgetc(file_input)) != EOF) {
+        if (strcasecmp("lower", method) == 0) {
+            if (isupper(ch)) {
+                ch = tolower(ch);
+                fputc(ch, file_output);
+            }
+        } else {
+            if (islower(ch)) {
+                ch = toupper(ch);
+                fputc(ch, file_output);
+            }
+        }
+    }
+
+    remove(file_name);
+    rename(file_name_temp, file_name);
+
+    free(file_name);
+    free(file_name_temp);
+    free(file_input);
+    free(file_output);
 
     return 0;
-
-
 
 }
